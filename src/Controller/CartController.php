34f616db1649ae639562a55cf8930f71d37592c8
Robin Rémy <remyrobin51@gmail.com>
2023-09-04 13:56:26 +0200

    :hammer: AJOUT/MODIF Panier
    Mise en place d'un affichage panier clair, avec les informations des produits.
    Calcul des totaux, sous-totaux.

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,13 +14,41 @@ use Symfony\Component\Routing\Annotation\Route;
 class CartController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(SessionInterface $session): Response
+    public function index(SessionInterface $session, ProductRepository $productRepository): Response
     {
         $cart = $session->get('panier', []);
+        $cartWithInfo = [];
+        $totalCart = 0.0;
+        if(!empty($cart)) {
+            $productInCart = [];
+
+            foreach ($cart as $idProduct => $qtyProduct) {
+                $productInCart[] = $idProduct;
+            }
+
+            $productsCart = $productRepository->findBy(['id' => $productInCart]);
+
+            foreach ($productsCart as $infoProduct) {
+                $cartWithInfo[$infoProduct->getId()] = array(
+                    'name' => $infoProduct->getProductName(),
+                    'pics' => $infoProduct->getPicturesUrls(),
+                    'qty' => $cart[$infoProduct->getId()]
+                );
+
+                if (is_null($infoProduct->getDiscountedPrice())) {
+                    $cartWithInfo[$infoProduct->getId()]['total'] = $infoProduct->getPrice() * $cartWithInfo[$infoProduct->getId()]['qty'];
+                } else {
+                    $cartWithInfo[$infoProduct->getId()]['total'] = $infoProduct->getDiscountedPrice() * $cartWithInfo[$infoProduct->getId()]['qty'];
+                }
+
+                $totalCart += $cartWithInfo[$infoProduct->getId()]['total'];
+            }
+        }
 
         return $this->render('cart/index.html.twig', [
             'controller_name' => 'CartController',
-            'panier' => $cart
+            'cart' => $cartWithInfo,
+            'total' => $totalCart
         ]);
     }
 #[Route('/add/{id}', name: 'add')]
