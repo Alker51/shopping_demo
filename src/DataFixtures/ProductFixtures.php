de0a98a4027352d7997fb5a3c20a1f5c1e40b0bb
Robin Rémy <remyrobin51@gmail.com>
2023-09-06 14:00:17 +0200

    :hammer: AJOUT : Produit "réel" pour les tests.
    Création en cours, d'un produit pour les tests.

--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Product;
 use App\Factory\ProductFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -10,6 +11,16 @@ class ProductFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-       $products = ProductFactory::createMany(10);
+        $product = new Product();
+        $product->setProductName('Peluche pêche.');
+        $product->setDescription('Magnifique peluche douce en forme de pêche. Elle saura vous tenir chaud et vous faire sourire avec ces petits pied et son large sourire.');
+        $product->setPicturesUrls('/public/img/peluche-peche.jpg');
+        $product->setStock(30);
+        $product->setDisplay(true);
+        $product->setPrice(30.5);
+
+        $manager->persist($product);
+        $manager->flush();
+        $products = ProductFactory::createMany(10);
     }
 }
