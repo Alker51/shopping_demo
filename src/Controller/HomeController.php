fe9cbddeeeda863d20a9f288a1b7f5862f661e6e
Robin Rémy <remyrobin51@gmail.com>
2023-09-15 14:06:01 +0200

    :hammer: AJOUT : WIP Récupération et stockage du panier.
    Si l'user se déconnecte, sauvegarde du panier, quand il se reconnecte restauration.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,23 @@
 
 namespace App\Controller;
 
-use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(SessionInterface $session): Response
     {
+        $user = $this->getUser();
+        if(!empty($user)) {
+            // $saveCart = ;
+            // $panier = $user->getSaveCart();
+            // $session->set('panier', $panier);
+        }
+
         return $this->render('home/index.html.twig', [
             'title' => 'Shopping_Demo',
         ]);
