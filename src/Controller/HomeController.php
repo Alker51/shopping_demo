706ed602631bef98202bb6933a59a31a6568410c
Robin Rémy <remyrobin51@gmail.com>
2023-09-20 13:48:30 +0200

    :wrench: MODIF : Mise à jour du chargement du panier sauvegarder.
    Chargement du panier sauvegarder dans LoginListener.php.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,31 +13,8 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(Session $session, UserRepository $userRepository): Response
     {
-        $cartLoad = $session->get('cartLoad', false);
-        $user = $this->getUser();
-
-        if(!is_null($cartLoad) && !$cartLoad && !empty($user)){
-            $this->loadSaveCart($userRepository, $session);
-            $session->set('cartLoad', true);
-        }
-
         return $this->render('home/index.html.twig', [
             'title' => 'Shopping_Demo',
         ]);
-    }
-
-    public function loadSaveCart(UserRepository $userRepository, Session $session): void
-    {
-        $user = $this->getUser();
-        if(!empty($user)) {
-            $connectedUsername = $user->getUserIdentifier();
-            $user = $userRepository->findOneBy(['username' => $connectedUsername]);
-            $saveCart = $user->getSaveCart();
-
-            $session->set('panier', empty($saveCart) ? [] : $saveCart);
-
-            $user->setSaveCart(null);
-            $userRepository->save($user, true);
-        }
     }
 }
--- src/EventListener/LoginListener.php
+++ src/EventListener/LoginListener.php
@@ -6,21 +6,23 @@ use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Component\HttpFoundation\Session\Session;
-use Symfony\Component\Security\Http\Event\LogoutEvent;
+use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
 
-class LogoutListener {
+class LoginListener
+{
 
     protected EntityManagerInterface $em;
 
     public function __construct(EntityManagerInterface $em)
     {
-        $this->em=$em;
+        $this->em = $em;
     }
-    public function onSymfonyComponentSecurityHttpEventLogoutEvent(LogoutEvent $event): void
+
+    public function onSymfonyComponentSecurityHttpEventLoginSuccessEvent(LoginSuccessEvent $event): void
     {
         $repository = $this->em->getRepository(User::class);
         $session = $event->getRequest()->getSession();
-        $this->saveCart($repository, $session, $event);
+        $this->loadCart($repository, $session, $event);
         /*
         If you need user...
         if (($token = $event->getToken()) && $user = $token->getUser()) {
@@ -29,17 +31,19 @@ class LogoutListener {
         */
     }
 
-    public function saveCart(EntityRepository $userRepository, Session $session, LogoutEvent $event): void
+    public function loadCart(EntityRepository $userRepository, Session $session, LoginSuccessEvent $event): void
     {
-        $cartToSave = $session->get('panier', []);
-        $token = $event->getToken();
+        $token = $event->getAuthenticatedToken();
         $user = $token->getUser();
 
-        if(!empty($user) && !empty($cartToSave)) {
+        if(!empty($user)) {
             $connectedUsername = $user->getUserIdentifier();
             $user = $userRepository->findOneBy(['username' => $connectedUsername]);
+            $saveCart = $user->getSaveCart();
 
-            $user->setSaveCart($cartToSave);
+            $session->set('panier', empty($saveCart) ? [] : $saveCart);
+
+            $user->setSaveCart(null);
             $userRepository->save($user, true);
         }
     }
