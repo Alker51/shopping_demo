80084290eac322479249cffbc1dadfbc434cf78a
Robin Rémy <remyrobin51@gmail.com>
2023-09-27 13:49:41 +0200

    :wrench: MODIF : Amélioration du template et de la methode create.
    Passage des infos pour créer la commande via la methode post.
    
    Récuperation des infos dans la methode create pour ensuite une création de la commande.

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -3,19 +3,20 @@
 namespace App\Controller;
 
 use App\Repository\OrderRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[Route('/order', name: 'app_order')]
+#[Route('/order', name: 'app_order_')]
 class OrderController extends AbstractController
 {
     #[Route('/', name: 'index')]
     #[IsGranted('ROLE_ADMIN', message: 'You are not allowed to manage Order.')]
     public function index(OrderRepository $orderRepository): Response
     {
-        $orders = $orderRepository->findBy([],['date' => 'ASC']);
+        $orders = $orderRepository->findBy([],['orderDate' => 'ASC']);
 
         return $this->render('order/index.html.twig', [
             'controller_name' => 'Commandes récentes.',
@@ -23,9 +24,14 @@ class OrderController extends AbstractController
         ]);
     }
 
-    #[Route(null, name: 'app_order_create')]
-    public function create(OrderRepository $orderRepository)
+    #[Route('/create', name: 'create')]
+    public function create(OrderRepository $orderRepository, UserRepository $userRepository)
     {
+        $user = $userRepository->findOneBy(['id' => $_POST['user']]);
+        var_dump($user->getUserIdentifier());
+        $cart = (array)json_decode($_POST['cart']);
+        var_dump($cart);
+        exit;
         // $order = new Order();
         // $order->
     }
