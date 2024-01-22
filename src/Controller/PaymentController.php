43e537efd8ec7e7dfbd6234238f6dad526294ce9
Rémy Robin <remyrobin51@gmail.com>
2024-01-22 19:30:10 +0100

    :hammer: ADD : Controller pour le paiement, script de bouton et template pour le paiement.

---
+++ src/Controller/PaymentController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/payment')]
+class PaymentController extends AbstractController
+{
+    #[Route('/', name: 'app_payment')]
+    public function index(): Response
+    {
+        return $this->render('payment/index.html.twig', [
+            'controller_name' => 'PaymentController',
+        ]);
+    }
+}
